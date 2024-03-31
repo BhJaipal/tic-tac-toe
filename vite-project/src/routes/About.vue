e1f5ad5f90f8f1c9b9a77cb6fe4e77f7bc2aa26f
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-03-31 11:06:10 +0530

    Trying components and organizing them

--- vite-project/src/routes/About.vue
+++ vite-project/src/routes/About.vue
@@ -1,319 +1,369 @@
 <template>
-	<v-card>
-		<v-tabs v-model="tabs" align-tabs="end" color="deep-purple-accent-4">
-			<v-tab :value="1">cards and tooltips</v-tab>
-			<v-tab :value="2">Accordions</v-tab>
-			<v-tab :value="3">Small size</v-tab>
-		</v-tabs>
-		<v-window v-model="tabs">
-			<v-window-item :value="1">
-				<v-container fluid>
-					<v-col>
-						<v-card elevation="2" color="#222" dark class="mt-2">
-							<v-card-title>{{ data.title }}</v-card-title>
-							<v-card-subtitle>{{
-								data.subtitle
-							}}</v-card-subtitle>
-							<v-card-text>{{ data.description }} </v-card-text>
-						</v-card>
-						<v-bottom-sheet inset>
-							<template v-slot:activator="{ props }">
-								<v-btn v-bind="props">Open Bottom sheet</v-btn>
-							</template>
-							<v-card>
-								<v-card-title class="text-h6">
-									Privacy Policy
-								</v-card-title>
-								<v-card-text
-									>Lorem ipsum dolor sit amet, consectetur
-									adipisicing elit. Nam quae, nulla sequi
-									adipisci dicta, voluptate aperiam aut
-									voluptatum inventore, blanditiis sunt
-									quisquam fuga rerum esse quas labore facilis
-									nostrum at.</v-card-text
-								>
-							</v-card>
-						</v-bottom-sheet>
-						<v-dialog persistent v-model="businessOffer">
-							<template v-slot:activator="{ props }">
-								<v-btn v-bind="props">Open Offer</v-btn>
-							</template>
-							<v-card prepend-icon="mdi-briefcase">
-								<v-card-title>Business Offer</v-card-title>
-								<v-card-text>
-									I recieve ... nothing <br />
-									You receive ... nothing
-								</v-card-text>
-								<template v-slot:actions>
-									<v-spacer></v-spacer>
-									<v-btn
-										@click="
-											{
-												businessOffer = false;
-												console.log(businessOffer);
-											}
-										"
-										>Reject</v-btn
-									>
-									<v-btn
-										@click="
-											{
-												businessOffer = false;
-												console.log(businessOffer);
-											}
-										"
-										>Accept</v-btn
-									>
-								</template>
-							</v-card>
-						</v-dialog>
-						<v-divider color="grey" thickness="10"></v-divider>
-						<v-pagination :length="5" rounded="xl"></v-pagination>
-					</v-col>
-				</v-container>
-				<v-col cols="12">
-					<v-combobox
-						v-model="selected"
-						:items="comboboxItems"
-						label="I use a scoped slot"
-						multiple
-					>
-						<template v-slot:selection="data">
-							<v-chip
-								:key="JSON.stringify(data.item)"
-								v-bind="data.attrs"
-								:disabled="data.disabled"
-								:model-value="data.selected"
-								size="small"
-								@click:close="data.parent.selectItem(data.item)"
+	<v-row align="center" justify="center">
+		<v-card width="1404">
+			<v-tabs
+				v-model="tabs"
+				align-tabs="end"
+				color="deep-purple-accent-4"
+			>
+				<v-tab :value="1">cards and tooltips</v-tab>
+				<v-tab :value="2">Accordions</v-tab>
+				<v-tab :value="3">Small size</v-tab>
+				<v-tab :value="4">Forms</v-tab>
+			</v-tabs>
+			<v-window v-model="tabs">
+				<v-window-item :value="1">
+					<v-container fluid>
+						<v-col>
+							<v-card
+								elevation="2"
+								color="#222"
+								dark
+								class="mt-2"
 							>
-								<template v-slot:prepend>
-									<v-avatar
-										class="bg-accent text-uppercase"
-										start
-										>{{
-											data.item.title.slice(0, 1)
-										}}</v-avatar
+								<v-card-title>{{ data.title }}</v-card-title>
+								<v-card-subtitle>{{
+									data.subtitle
+								}}</v-card-subtitle>
+								<v-card-text
+									>{{ data.description }}
+								</v-card-text>
+							</v-card>
+							<v-bottom-sheet inset>
+								<template v-slot:activator="{ props }">
+									<v-btn v-bind="props"
+										>Open Bottom sheet</v-btn
 									>
 								</template>
-								{{ data.item.title }}
-							</v-chip>
-						</template>
-					</v-combobox>
-				</v-col>
-			</v-window-item>
-			<v-window-item :value="2">
-				<v-container fluid>
-					<v-col>
-						<v-expansion-panels>
-							<v-expansion-panel>
-								<v-expansion-panel-title
-									collapse-icon="mdi-minus"
-									expand-icon="mdi-plus"
-								>
-									Item
-								</v-expansion-panel-title>
-								<v-expansion-panel-text>
-									Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit, sed do eiusmod tempor
-									incididunt ut labore et dolore magna aliqua.
-									Ut enim ad minim veniam, quis nostrud
-									exercitation ullamco laboris nisi ut aliquip
-									ex ea commodo consequat.
-								</v-expansion-panel-text>
-							</v-expansion-panel>
-
-							<v-expansion-panel>
-								<v-expansion-panel-title>
-									Item
-									<template v-slot:actions="{ expanded }">
-										<v-icon
-											:color="!expanded ? 'teal' : ''"
-											:icon="
-												expanded
-													? 'mdi-pencil'
-													: 'mdi-check'
-											"
-										></v-icon>
-									</template>
-								</v-expansion-panel-title>
-								<v-expansion-panel-text>
-									Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit, sed do eiusmod tempor
-									incididunt ut labore et dolore magna aliqua.
-									Ut enim ad minim veniam, quis nostrud
-									exercitation ullamco laboris nisi ut aliquip
-									ex ea commodo consequat.
-								</v-expansion-panel-text>
-							</v-expansion-panel>
-
-							<v-expansion-panel>
-								<v-expansion-panel-title disable-icon-rotate>
-									Item
+								<v-card>
+									<v-card-title class="text-h6">
+										Privacy Policy
+									</v-card-title>
+									<v-card-text
+										>Lorem ipsum dolor sit amet, consectetur
+										adipisicing elit. Nam quae, nulla sequi
+										adipisci dicta, voluptate aperiam aut
+										voluptatum inventore, blanditiis sunt
+										quisquam fuga rerum esse quas labore
+										facilis nostrum at.</v-card-text
+									>
+								</v-card>
+							</v-bottom-sheet>
+							<v-dialog persistent v-model="businessOffer">
+								<template v-slot:activator="{ props }">
+									<v-btn v-bind="props">Open Offer</v-btn>
+								</template>
+								<v-card prepend-icon="mdi-briefcase">
+									<v-card-title>Business Offer</v-card-title>
+									<v-card-text>
+										I recieve ... nothing <br />
+										You receive ... nothing
+									</v-card-text>
 									<template v-slot:actions>
-										<v-icon
-											color="error"
-											icon="mdi-alert-circle"
+										<v-spacer></v-spacer>
+										<v-btn
+											@click="
+												{
+													businessOffer = false;
+													console.log(businessOffer);
+												}
+											"
+											>Reject</v-btn
 										>
-										</v-icon>
+										<v-btn
+											@click="
+												{
+													businessOffer = false;
+													console.log(businessOffer);
+												}
+											"
+											>Accept</v-btn
+										>
 									</template>
-								</v-expansion-panel-title>
-								<v-expansion-panel-text>
-									Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit, sed do eiusmod tempor
-									incididunt ut labore et dolore magna aliqua.
-									Ut enim ad minim veniam, quis nostrud
-									exercitation ullamco laboris nisi ut aliquip
-									ex ea commodo consequat.
-								</v-expansion-panel-text>
-							</v-expansion-panel>
-						</v-expansion-panels>
-						<v-list density="compact">
-							<v-list-subheader>REPORTS</v-list-subheader>
+								</v-card>
+							</v-dialog>
+							<v-divider color="grey" thickness="10"></v-divider>
+							<v-pagination
+								:length="5"
+								rounded="xl"
+							></v-pagination>
+						</v-col>
+					</v-container>
+					<v-col cols="12">
+						<v-combobox
+							v-model="selected"
+							:items="comboboxItems"
+							label="I use a scoped slot"
+							multiple
+						>
+							<template v-slot:selection="data">
+								<v-chip
+									:key="JSON.stringify(data.item)"
+									size="small"
+								>
+									<template v-slot:prepend>
+										<v-avatar
+											class="bg-accent text-uppercase"
+											start
+											>{{
+												data.item.title.slice(0, 1)
+											}}</v-avatar
+										>
+									</template>
+									{{ data.item.title }}
+								</v-chip>
+							</template>
+						</v-combobox>
+					</v-col>
+				</v-window-item>
+				<v-window-item :value="2">
+					<v-container fluid>
+						<v-col>
+							<v-expansion-panels>
+								<v-expansion-panel>
+									<v-expansion-panel-title
+										collapse-icon="mdi-minus"
+										expand-icon="mdi-plus"
+									>
+										Item
+									</v-expansion-panel-title>
+									<v-expansion-panel-text>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dolore magna
+										aliqua. Ut enim ad minim veniam, quis
+										nostrud exercitation ullamco laboris
+										nisi ut aliquip ex ea commodo consequat.
+									</v-expansion-panel-text>
+								</v-expansion-panel>
 
-							<v-list-item
-								v-for="(item, i) in items"
-								:key="i"
-								:value="item"
-								color="primary"
+								<v-expansion-panel>
+									<v-expansion-panel-title>
+										Item
+										<template v-slot:actions="{ expanded }">
+											<v-icon
+												:color="!expanded ? 'teal' : ''"
+												:icon="
+													expanded
+														? 'mdi-pencil'
+														: 'mdi-check'
+												"
+											></v-icon>
+										</template>
+									</v-expansion-panel-title>
+									<v-expansion-panel-text>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dolore magna
+										aliqua. Ut enim ad minim veniam, quis
+										nostrud exercitation ullamco laboris
+										nisi ut aliquip ex ea commodo consequat.
+									</v-expansion-panel-text>
+								</v-expansion-panel>
+
+								<v-expansion-panel>
+									<v-expansion-panel-title
+										disable-icon-rotate
+									>
+										Item
+										<template v-slot:actions>
+											<v-icon
+												color="error"
+												icon="mdi-alert-circle"
+											>
+											</v-icon>
+										</template>
+									</v-expansion-panel-title>
+									<v-expansion-panel-text>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dolore magna
+										aliqua. Ut enim ad minim veniam, quis
+										nostrud exercitation ullamco laboris
+										nisi ut aliquip ex ea commodo consequat.
+									</v-expansion-panel-text>
+								</v-expansion-panel>
+							</v-expansion-panels>
+							<v-list density="compact">
+								<v-list-subheader>REPORTS</v-list-subheader>
+
+								<v-list-item
+									v-for="(item, i) in items"
+									:key="i"
+									:value="item"
+									color="primary"
+									rounded="xl"
+								>
+									<template v-slot:prepend>
+										<v-icon :icon="item.icon"></v-icon>
+									</template>
+
+									<v-list-item-title
+										:v-text="item.text"
+									></v-list-item-title>
+								</v-list-item>
+							</v-list>
+							<v-sheet
+								:elevation="24"
+								:height="100"
+								:width="300"
+								border
+								color="success"
 								rounded="xl"
 							>
-								<template v-slot:prepend>
-									<v-icon :icon="item.icon"></v-icon>
-								</template>
-
-								<v-list-item-title
-									:v-text="item.text"
-								></v-list-item-title>
-							</v-list-item>
-						</v-list>
-						<v-sheet
-							:elevation="24"
-							:height="100"
-							:width="300"
-							border
-							color="success"
-							rounded="xl"
+								<p class="mt-2">This is a sheet</p>
+							</v-sheet></v-col
 						>
-							<p class="mt-2">This is a sheet</p>
-						</v-sheet></v-col
-					>
-				</v-container>
-			</v-window-item>
-			<v-window-item :value="3">
-				<v-container :fluid="true">
-					<v-col>
-						<v-btn variant="tonal"> Button </v-btn>
-						<div class="chip-div">
-							<v-chip
-								prepend-icon="$vuetify"
-								variant="elevated"
-								elevation="10"
-							>
-								Chip
-							</v-chip>
-							<v-chip
-								append-avatar="/vite.svg"
-								variant="outlined"
-							>
-								Chip
-							</v-chip>
-							<v-chip
-								prepend-avatar="/vue.svg"
-								closable
-								:model-value="chip"
-								close-icon="mdi-delete"
-								@click:close="chip = false"
-								variant="tonal"
-							>
-								Chip
-							</v-chip>
-
-							<v-btn
-								v-if="!chip"
-								color="primary"
-								close
-								dark
-								@click="chip = true"
-								>Reset Chip
-							</v-btn>
-						</div>
-
-						<v-row>
-							<v-btn @click="tooltipShow = !tooltipShow">
-								Button
-							</v-btn>
-							<v-tooltip v-model="tooltipShow" location="top">
-								<template v-slot:activator="{ props }">
-									<v-btn icon v-bind="props">
-										<v-icon color="grey-lighten-1">
-											mdi-cart
-										</v-icon>
-									</v-btn>
-								</template>
-								<span>Programmatic tooltip</span>
-							</v-tooltip>
-						</v-row>
-						<v-row>
-							<v-breadcrumbs :items="breadcrumbeRoute">
-								<template v-slot:divider>
-									<v-icon icon="mdi-chevron-right"></v-icon>
-								</template>
-							</v-breadcrumbs>
-						</v-row>
-						<div class="div">
-							<v-toolbar
-								title="Toolbar"
-								:elevation="10"
-								density="compact"
-							>
-								<v-toolbar-items>
-									<v-btn
-										variant="outlined"
-										icon="mdi-menu"
-									></v-btn>
-								</v-toolbar-items>
-							</v-toolbar>
-						</div>
-						<v-file-input
-							:rules="[
-								(value) => {
-									return (
-										!value ||
-										!value.length ||
-										value[0].size < 2000000 ||
-										'Avatar size should be less than 2 MB!'
-									);
-								},
-							]"
-							:show-size="1024"
-							:counter="true"
-							accept="image/png, image/jpeg, image/bmp"
-							label="Avatar"
-							placeholder="Pick an avatar"
-							prepend-icon="mdi-camera"
-						>
-							<template v-slot:selection="{ fileNames }">
-								<template
-									v-for="fileName in fileNames"
-									:key="fileName"
-								>
+					</v-container>
+				</v-window-item>
+				<v-window-item :value="3">
+					<v-container :fluid="true">
+						<v-col>
+							<v-btn variant="tonal"> Button </v-btn>
+							<v-row class="mt-10" no-gutters>
+								<v-col cols="12" sm="4">
 									<v-chip
-										class="me-2"
-										color="primary"
-										size="small"
-										label
+										prepend-icon="$vuetify"
+										variant="elevated"
+										elevation="10"
 									>
-										{{ fileName }}
+										Chip
 									</v-chip>
-								</template></template
+								</v-col>
+								<v-col cols="12" sm="4">
+									<v-chip
+										append-avatar="/vite.svg"
+										variant="outlined"
+									>
+										Chip
+									</v-chip></v-col
+								>
+								<v-col cols="12" sm="4">
+									<v-chip
+										prepend-avatar="/vue.svg"
+										closable
+										:model-value="chip"
+										close-icon="mdi-delete"
+										@click:close="chip = false"
+										variant="tonal"
+									>
+										Chip
+									</v-chip>
+
+									<v-btn
+										v-if="!chip"
+										color="primary"
+										close
+										dark
+										@click="chip = true"
+										>Reset Chip
+									</v-btn></v-col
+								>
+							</v-row>
+
+							<v-row class="flex justify-center mt-5">
+								<v-btn @click="tooltipShow = !tooltipShow">
+									Button
+								</v-btn>
+								<v-tooltip v-model="tooltipShow" location="top">
+									<template v-slot:activator="{ props }">
+										<v-btn icon v-bind="props">
+											<v-icon color="grey-lighten-1">
+												mdi-cart
+											</v-icon>
+										</v-btn>
+									</template>
+									<span>Programmatic tooltip</span>
+								</v-tooltip>
+							</v-row>
+							<v-row>
+								<v-breadcrumbs :items="breadcrumbeRoute">
+									<template v-slot:divider>
+										<v-icon
+											icon="mdi-chevron-right"
+										></v-icon>
+									</template>
+								</v-breadcrumbs>
+							</v-row>
+							<div class="div">
+								<v-toolbar
+									title="Toolbar"
+									:elevation="10"
+									density="compact"
+								>
+									<v-toolbar-items>
+										<v-btn
+											variant="outlined"
+											icon="mdi-menu"
+										></v-btn>
+									</v-toolbar-items>
+								</v-toolbar>
+							</div>
+							<v-file-input
+								:rules="[
+									(value) => {
+										return (
+											!value ||
+											!value.length ||
+											value[0].size < 2000000 ||
+											'Avatar size should be less than 2 MB!'
+										);
+									},
+								]"
+								:show-size="1024"
+								:counter="true"
+								accept="image/png, image/jpeg, image/bmp"
+								label="Avatar"
+								placeholder="Pick an avatar"
+								prepend-icon="mdi-camera"
 							>
-						</v-file-input>
-					</v-col>
-				</v-container>
-			</v-window-item>
-		</v-window>
-	</v-card>
+								<template v-slot:selection="{ fileNames }">
+									<template
+										v-for="fileName in fileNames"
+										:key="fileName"
+									>
+										<v-chip
+											class="me-2"
+											color="primary"
+											size="small"
+											label
+										>
+											{{ fileName }}
+										</v-chip>
+									</template></template
+								>
+							</v-file-input>
+						</v-col>
+					</v-container>
+				</v-window-item>
+				<v-window-item :value="4">
+					<v-form @submit="submitForm">
+						<v-otp-input v-model="formData.otp"></v-otp-input>
+						<v-btn type="submit" class="mb-4">Submit</v-btn>
+					</v-form>
+					<v-divider color="grey" thickness="10"></v-divider>
+					<template v-if="formData.out != ''">
+						<v-row justify="center" align="center" class="my-10">
+							<template v-if="formData.out == 'correct otp'">
+								<v-card color="success" width="300">
+									<v-card-text>
+										{{ formData.out }}
+									</v-card-text>
+								</v-card>
+							</template>
+							<template v-else>
+								<v-card color="error" width="300">
+									<v-card-text>
+										{{ formData.out }}
+									</v-card-text>
+								</v-card>
+							</template>
+						</v-row>
+					</template>
+				</v-window-item>
+			</v-window>
+		</v-card>
+	</v-row>
 	<v-bottom-navigation v-model="BtmNavValue" color="primary" horizontal>
 		<v-btn>
 			<v-icon>mdi-history</v-icon>
@@ -344,7 +394,24 @@ let breadcrumbeRoute = route.path.split("/").map((it) => {
 	return { title: it, disabled: "/" + it == route.path, href: "/" + it };
 });
 breadcrumbeRoute[0].title = "/";
-console.log(breadcrumbeRoute);
+
+let formData = ref({
+	name: "",
+	email: "",
+	message: "",
+	otp: 0,
+	correctOtp: 777777,
+	out: "",
+});
+
+function submitForm(e: any) {
+	e.preventDefault();
+	if (formData.value.otp == formData.value.correctOtp) {
+		formData.value.out = "correct otp";
+	} else {
+		formData.value.out = "wrong otp";
+	}
+}
 
 let items = [
 	{ text: "Real-Time", icon: "mdi-clock" },
@@ -362,14 +429,6 @@ let data = {
 <style scoped>
 .mt-2 {
 	margin-top: 2rem;
-}
-.div > .chip-div {
-	display: flex;
-	flex-direction: row;
-	margin: 0px;
-	padding: 0%;
-	width: 100%;
-	row-gap: 20px;
 }
 .div {
 	height: 100%;
